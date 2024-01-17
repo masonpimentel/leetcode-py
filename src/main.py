8867877cd5ff55cdd5adbbab4f45de05057a7c65
Mason Pimentel <mason@mpimentel.net>
2024-01-16 18:47:06 -0800

    Get specific question

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ def main():
             "filters": {
                 "difficulty": "MEDIUM",
                 "orderBy": "FRONTEND_ID",
-                "sortOrder": "ASCENDING"
+                "sortOrder": "ASCENDING",
             },
         },
         "operationName": "problemsetQuestionList",
@@ -67,8 +67,30 @@ def main():
         timeout=10,
     )
     questions = response.json()["data"]["problemsetQuestionList"]["questions"]
-    questions = list(filter(lambda question: question['status'] != 'ac', questions))
-    print(f'Total problems: {len(questions)}')
+    questions = list(filter(lambda question: question["status"] != "ac", questions))
+    print(f"Total problems: {len(questions)}")
+
+    print(questions[0]['titleSlug'])
+
+    slug = questions[0]['titleSlug']
+
+    json_data = {
+        'operationName': 'questionData',
+        'variables': {
+            'titleSlug': slug,
+        },
+        'query': 'query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    canSeeQuestion\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    exampleTestcases\n    categoryTitle\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      paidOnlyVideo\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    challengeQuestion {\n      id\n      date\n      incompleteChallengeCount\n      streakCount\n      type\n      __typename\n    }\n    __typename\n  }\n}\n',
+    }
+    response = requests.post(
+        "https://leetcode.com/graphql/",
+        cookies=cookies,
+        headers=headers,
+        json=json_data,
+        timeout=10,
+    )
+    print(response.json()['data']['question']['likes'])
+    print(response.json()['data']['question']['dislikes'])
+
 
 if __name__ == "__main__":
     main()
