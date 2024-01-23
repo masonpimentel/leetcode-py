8f3ac6b1147e41b38158912c2372729eeb20fef8
Mason Pimentel <mason@mpimentel.net>
2024-01-22 22:30:17 -0800

    Test call for all questions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,38 @@
 from src import main
+import requests
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
-def test_answer():
-    assert main.inc(3) == 4
+
+@patch('builtins.print')
+def test_answer(mock_print):
+    requests.post = MagicMock()
+
+    class MockQuestionResponse:
+        def json(self):
+            return {
+                'data': {
+                    'problemsetQuestionList': {
+                        'questions': [
+                            {
+                                'status': 'ac'
+                            },
+                            {
+                                'status': 'notcomplete'
+                            },
+                            {
+                                'status': 'notcomplete'
+                            },
+                        ]
+                    }
+                }
+            }
+
+    requests.post.side_effect = [MockQuestionResponse()]
+
+    main.print_problems()
+    mock_print.assert_called_with('Total problems: 2')
+
+    # print(res)
+    # assert False
+    
