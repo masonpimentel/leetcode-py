6171fa1e8d3d5fe388edee42df895bda8b3150c1
Mason Pimentel <me@masonpimentel.com>
2024-06-10 15:10:28 -0700

    Print slug instead of title

--- src/main.py
+++ src/main.py
@@ -69,7 +69,6 @@ def print_problems() -> None:
             "query": "query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    canSeeQuestion\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    exampleTestcases\n    categoryTitle\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      paidOnlyVideo\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    challengeQuestion {\n      id\n      date\n      incompleteChallengeCount\n      streakCount\n      type\n      __typename\n    }\n    __typename\n  }\n}\n",
         }
 
-            print(questionFromDetail["title"])
         try:
             time.sleep(SLEEP_TIME)
 
@@ -104,6 +103,7 @@ def print_problems() -> None:
                 and questionFromDetail["codeSnippets"]
                 and LANGUAGE in snippets
             ):
+                print(f'https://leetcode.com/problems/{questionFromDetail["titleSlug"]}')
             
             i += 1
         except:
